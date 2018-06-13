1a11b1308748331b76701f9a99a01d37f4d73393
Regan Gilbert <regan.m.gilbert@gmail.com>
2018-06-13 13:20:15 -0600

    setup initial state for card components

--- src/components/card.js
+++ src/components/card.js
@@ -3,12 +3,38 @@ import React, { Component } from "react";
 import Input from './input';
 
 class Card extends Component {
-  render() {
+
+  constructor() {
+    super()
+
+    this.state= {
+      color: 'Blue',
+      pluralNoun: ''
+    }
+
+  }
+
+  handleInputChange() {
+    this.setState({ color: 'red'})
+  }
+
+
+  render() {  
+    
     return (
       <div className="card">
-        { Input('Color') }
-        { Input('Plural Noun') }
+      <h1>{this.state.color}</h1>
+      <input/>
 
+      </div>
+    )
+
+    return (
+      
+      <div className="card">
+      <h1>{this.state.color}</h1>
+        { Input('Color') }
+        { Input('pluralNoun') }
         </div>
     )
 
